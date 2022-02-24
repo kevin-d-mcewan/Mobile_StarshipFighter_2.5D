92802159e4d55ece1ee12975be138c229a9d3a55
kevin-d-mcewan <MacKevinProgramming@gmail.com>
2022-02-24 08:36:31 -0800

    Added some movement to game. Need to fine tune

---
+++ Assets/Allegorithmic/Plugins/Substance.EditorHelper/Scripter.cs
@@ -0,0 +1,182 @@
+﻿
+using System;
+using UnityEditor;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+
+namespace Substance.EditorHelper
+{
+    public class Scripter
+    {
+        public static void Hello()
+        {
+            Debug.Log("Scripter's Hello");
+
+#if UNITY_2019_3_OR_NEWER
+            Debug.Log("UNITY_2019_3_OR_NEWER");
+#endif
+        }
+
+        public static class UnityPipeline
+        {
+            // The active project context is in the 'Edit->Project Settings->Graphics->Scriptable Render Pipeline Settings' field.
+            public static bool IsHDRP()
+            {
+#if UNITY_2019_3_OR_NEWER
+            bool bActive = false;
+
+            UnityEngine.Rendering.RenderPipelineAsset asset;
+            asset = UnityEngine.Rendering.GraphicsSettings.renderPipelineAsset;
+
+            if ((asset != null) &&
+                (asset.GetType().ToString().EndsWith(".HDRenderPipelineAsset")))
+            {
+                    bActive = true;
+            }
+
+            return bActive;
+#else
+                return false;
+#endif
+            }
+
+            public static bool IsURP()
+            {
+#if UNITY_2019_3_OR_NEWER
+                bool bActive = false;
+
+                UnityEngine.Rendering.RenderPipelineAsset asset;
+                asset = UnityEngine.Rendering.GraphicsSettings.renderPipelineAsset;
+
+                if ((asset != null) &&
+                    (asset.GetType().ToString().EndsWith("UniversalRenderPipelineAsset")))
+                {
+                    bActive = true;
+                }
+
+                return bActive;
+#else
+                return false;
+#endif
+            }
+
+            // Keep the following for PackageManager investigation:
+            /*
+            public static UnityEditor.PackageManager.PackageInfo GetPackageInfo(string pName)
+            {
+#if UNITY_2019_3_OR_NEWER
+                Debug.Log("New stuff...");
+
+                UnityEditor.PackageManager.PackageInfo info;
+
+                info = UnityEditor.PackageManager.PackageInfo.FindForAssetPath(pName);
+
+                return info;
+#else
+                Debug.Log("Old stuff...");
+                return null;
+#endif
+            }
+            */
+        }
+
+        // =================================================================================
+        // SendTo: launch launcher:
+        [DllImport("substance_portal_cli", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        public static extern uint substance_portal_cli_invoke();
+
+        // ---------------------------------------------------------------------------------
+        // The following 'Shader' functions are only used for Unity2019.3 & up.
+        // (so, for compilation purposes, with pre-Unity2019.3 APIs: we have to implement a dummy version of each)
+
+#if UNITY_2019_3_OR_NEWER
+        public enum ShaderPropertyType
+        {
+            Color = UnityEngine.Rendering.ShaderPropertyType.Color,
+            Float = UnityEngine.Rendering.ShaderPropertyType.Float
+        }
+#else
+        public enum ShaderPropertyType
+        {
+            Invalid = -1,
+            Color = -2,
+            Float = -3
+        }
+#endif
+
+#if UNITY_2019_3_OR_NEWER
+        public static int GetPropertyCount(Shader pShader)
+        {
+            return pShader.GetPropertyCount();
+        }
+#else
+        public static int GetPropertyCount(Shader pShader)
+        {
+            Debug.LogWarning(string.Format("Unexpected Shader function call: '{0}'!",
+                System.Reflection.MethodBase.GetCurrentMethod().Name));
+            return 0;
+        }
+#endif
+
+#if UNITY_2019_3_OR_NEWER
+        public static string GetPropertyName(Shader pShader, int pIndex)
+        {
+            return pShader.GetPropertyName(pIndex);
+        }
+#else
+        public static string GetPropertyName(Shader pShader, int pIndex)
+        {
+            Debug.LogWarning(string.Format("Unexpected Shader function call: '{0}'!",
+                System.Reflection.MethodBase.GetCurrentMethod().Name));
+            return string.Empty;
+        }
+#endif
+
+#if UNITY_2019_3_OR_NEWER
+        public static ShaderPropertyType GetPropertyType(Shader pShader, int pIndex)
+        {
+            return (ShaderPropertyType)pShader.GetPropertyType(pIndex);
+        }
+#else
+        public static ShaderPropertyType GetPropertyType(Shader pShader, int pIndex)
+        {
+            Debug.LogWarning(string.Format("Unexpected Shader function call: '{0}'!",
+                System.Reflection.MethodBase.GetCurrentMethod().Name));
+            return ShaderPropertyType.Invalid;
+        }
+#endif
+
+#if UNITY_2019_3_OR_NEWER
+        public static float GetPropertyDefaultFloatValue(Shader pShader, int pIndex)
+        {
+            return pShader.GetPropertyDefaultFloatValue(pIndex);
+        }
+#else
+        public static float GetPropertyDefaultFloatValue(Shader pShader, int pIndex)
+        {
+            Debug.LogWarning(string.Format("Unexpected Shader function call: '{0}'!",
+                System.Reflection.MethodBase.GetCurrentMethod().Name));
+            return 0;
+        }
+#endif
+
+#if UNITY_2019_3_OR_NEWER
+        public static Color GetPropertyDefaultVectorValue(Shader pShader, int pIndex)
+        {
+            return pShader.GetPropertyDefaultVectorValue(pIndex);
+        }
+#else
+        public static Color GetPropertyDefaultVectorValue(Shader pShader, int pIndex)
+        {
+            Debug.LogWarning(string.Format("Unexpected Shader function call: '{0}'!",
+                System.Reflection.MethodBase.GetCurrentMethod().Name));
+            return new Color(0,0,0,0);
+        }
+#endif
+
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,12 +5,19 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float forceMagnitude;
+    [SerializeField] private float maxVelo;
 
+
+    private Rigidbody rb;
     private Camera mainCamera;
+
+    private Vector3 movementDirection;
     
     void Start()
     {
         mainCamera = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
     
@@ -21,13 +28,45 @@ public class PlayerMovement : MonoBehaviour
         {
             Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-            Debug.Log(touchPosition);
+            // Debug.Log(touchPosition);
 
             Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
 
-            Debug.Log(worldPosition);
+            //Debug.Log(worldPosition);
+
+            // This is player position (transform.position since this script is attached to player) - where we touched (worldPosition)
+            /* This moves around like INVERTED CONTROLS so may need to tweak this to "movementDirection = worldPosition - transform.position" or something similiar*/
+            movementDirection = transform.position - worldPosition;
+            movementDirection.z = 0f;
+            /* We will use Normalize() here bc that means we dont care about how far away we touched or hard just the direction and "Normalize()" does that
+             by returning a 'Maginatude' of 1*/
+            movementDirection.Normalize();
+        }
+        else
+        {
+            /* This says if we aren't touching then dont add anything to it; And if we release our finger how fast it slows down depends on the DRAG on the
+             * RIGIDBODY that is attached. So this can be adjusted via code or inspector*/
+            movementDirection = Vector3.zero;
         }
 
+    }
 
+    // We need to use FixedUpdate bc we are using Physics which is updated every physics update (which is not every frame like Update)
+    private void FixedUpdate()
+    {
+        // if the movementDirection is Zero just return out of statement
+        if (movementDirection == Vector3.zero)
+        {
+            return;
+        }
+
+        // If it isn't Zero then we want to AddForce to the movement
+        /* We dont add "Time.deltaTime" after 'forceMagnitude' bc since it is in FIxedUpdate() its already called a specific amount of time,
+         * do to this we may need to cut the forceMagnitude by half(divide by 50)*/
+        /* Also we want to make sure the velocity doesn't go over our defined maximum which can happed if Vector3.ClampMagnitude is not used because it will continue
+         * to add Magnitude to it overtime as you continue to press down on screen*/
+        rb.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelo);
     }
 }
